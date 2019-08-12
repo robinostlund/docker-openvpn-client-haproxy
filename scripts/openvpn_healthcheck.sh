6c217468c9d2308f85ebd2f69e0102ab851f4a2f
Robin Östlund <me@robinostlund.name>
2019-08-12 12:12:27 +0200

    updates

--- scripts/openvpn_healthcheck.sh
+++ scripts/openvpn_healthcheck.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-python /usr/bin/openvpn_healthcheck.py >> /vpn/log/openvpn_healthcheck.log
+python /usr/bin/openvpn_healthcheck.py >> /data/openvpn/log/openvpn_healthcheck.log
 OPENVPN_HEALTHCHECK_EXIT_CODE=$?
 
 exit $OPENVPN_EXIT_CODE
